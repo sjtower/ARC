9040d17258061c8b989785457321d3af249020a6
atsoy04 <atsoy@zendesk.com>
2015-08-26 21:12:47 -0700

    adding comment

--- GithubCommitReader.py
+++ GithubCommitReader.py
@@ -5,6 +5,7 @@ __author__ = 'Simon'
 import requests
 import json
 
+#make request to the commit URL and print response
 myData = requests.get('https://api.github.com/repos/sjtower/ARC/git/commits/fae79f8147d34fbbd876bc4822122d116055d3ac').json()
 print(json.dumps(myData, indent=2))
 # print json.dumps(parsed, indent=4, sort_keys=True)
