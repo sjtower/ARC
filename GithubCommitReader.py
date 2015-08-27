b2fd6eb02bc7bf6e81a6ece3e0505c01372a3d4b
sjtower <simon.tower@gmail.com>
2015-08-26 21:06:33 -0700

    print GET response for Github initial commit of ARC

--- GithubCommitReader.py
+++ GithubCommitReader.py
@@ -2,3 +2,9 @@ __author__ = 'Simon'
 
 # Initial file to test reading a single commit from a Github repository#
 
+import requests
+import json
+
+myData = requests.get('https://api.github.com/repos/sjtower/ARC/git/commits/fae79f8147d34fbbd876bc4822122d116055d3ac').json()
+print(json.dumps(myData, indent=2))
+# print json.dumps(parsed, indent=4, sort_keys=True)
