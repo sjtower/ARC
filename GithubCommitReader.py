d8ff9134e4d6bdde1f91195eeca4dba44e33ffc0
sjtower <simon.tower@gmail.com>
2016-02-01 20:51:49 -0800

    Try a diff api call

--- GithubCommitReader.py
+++ GithubCommitReader.py
@@ -5,6 +5,10 @@ __author__ = 'Simon'
 import requests
 import json
 
-myData = requests.get('https://api.github.com/repos/sjtower/ARC/git/commits/fae79f8147d34fbbd876bc4822122d116055d3ac').json()
-print(json.dumps(myData, indent=2))
-# print json.dumps(parsed, indent=4, sort_keys=True)
+headers = {'Accept': 'application/vnd.github.diff'}
+
+url = 'https://api.github.com/repos/sjtower/ARC/commits/fae79f8147d34fbbd876bc4822122d116055d3ac'
+# url = 'https://api.github.com/repos/pengwynn/dotfiles/commits/aee60a4cd56fb4c6a50e60f17096fc40c0d4d72c'
+myData = requests.get(url, headers=headers)
+# print(json.dumps(myData, indent=2))
+print myData.text
