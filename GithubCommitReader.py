8e5a2ff8f8d5ba2877e34017efc1e3907497776d
sjtower <simon.tower@gmail.com>
2016-02-01 21:48:31 -0800

    Consider only new lines in diff

--- GithubCommitReader.py
+++ GithubCommitReader.py
@@ -9,8 +9,18 @@ import json
 
 headers = {'Accept': 'application/vnd.github.diff'}
 
-url = 'https://api.github.com/repos/sjtower/ARC/commits/fae79f8147d34fbbd876bc4822122d116055d3ac'
-# url = 'https://api.github.com/repos/pengwynn/dotfiles/commits/aee60a4cd56fb4c6a50e60f17096fc40c0d4d72c'
+# url = 'https://api.github.com/repos/sjtower/ARC/commits/38518f0354928340c866fc2e4befb2191678fd21'
+# url = 'https://api.github.com/repos/hitherejoe/LeanbackCards/commits/89442c828d29a4335a7a1256ba4ba751b8aa72a4'
+
+url = 'https://api.github.com/repos/elastic/elasticsearch/commits/d91a898f6a339a48d546d9205bcbd7c09a390631'
 myData = requests.get(url, headers=headers)
 # print(json.dumps(myData, indent=2))
-print myData.text
+# print myData.text
+
+text_split = myData.text.split('\n')
+for line in text_split:
+    if line.startswith('+'):
+        if '=' in line:
+            line_split = line.split('=')
+            term = line_split[0].split()[-1]
+            print term
